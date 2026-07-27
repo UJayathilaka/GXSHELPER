f2badb3b1c78ab5059bbf8c9c2d90b61fb0add21
UJayathilaka <udajayathilaka21@gmail.com>
2026-07-27 22:25:38 +0530

    Add help button functionality to display message

---
+++ script.js
@@ -0,0 +1,10 @@
+let button = document.getElementById("helpButton");
+
+let text = document.getElementById("description");
+
+
+button.onclick = function() {
+
+    text.innerHTML = "Mapper support tools are ready!";
+
+};
