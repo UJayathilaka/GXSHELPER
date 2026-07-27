b760007dfa118cc3ac54659b1480324a05853bc4
UJayathilaka <udajayathilaka21@gmail.com>
2026-07-27 22:52:43 +0530

    Replace button functionality with data fetching
    Removed button functionality and added fetch to load items from data.json.

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
-let button = document.getElementById("helpButton");
-
-let text = document.getElementById("description");
-
-
-button.onclick = function() {
-
-    text.innerHTML = "Mapper support tools are ready!";
-
-};
+fetch('data.json')
+  .then(response => response.json())
+  .then(items => {
+    const container = document.getElementById('items');
+    items.forEach(item => {
+      container.innerHTML += `
+        <div class="card">
+          <img src="${item.image}" alt="${item.title}">
+          <h3>${item.title}</h3>
+          <p>${item.description}</p>
+        </div>
+      `;
+    });
+  });
